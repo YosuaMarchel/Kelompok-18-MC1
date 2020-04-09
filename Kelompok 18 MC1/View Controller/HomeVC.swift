f2da7cfbff948c8e4d70756c92cd2f7498503097
Yosua Marchel <yosua.marchel@gmail.com>
2020-04-09 19:00:13 +0700

    make the timer at home storyboard

--- Kelompok 18 MC1/View Controller/HomeVC.swift
+++ Kelompok 18 MC1/View Controller/HomeVC.swift
@@ -9,22 +9,133 @@
 import UIKit
 
 class HomeVC: UIViewController {
-
+    
+    @IBOutlet weak var topTextLabel: UILabel!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var resumeButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var timerProgress: UIProgressView!
+    
+    var seconds: Int64 = 0 //This variable will hold a starting value of seconds. It could be any amount above 0.
+    var timer = Timer()
+    var isTimerRunning = false //This will be used to make sure only one timer is created at a time.
+    var resumeTapped = false
+    var progress = Progress(totalUnitCount: 0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        pauseButton.isEnabled = false
+        pauseButton.isHidden = true
+        resumeButton.isHidden = true
+        slider.isHidden = false
+        timerProgress.isHidden = true
+        resetButton.isHidden = true
+        topTextLabel.text = "Set the timer"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func sliderValueChange(_ sender: UISlider) {
+        //        let currentValue = Int(sender.value)
+        
+        let currentValue = Int64(sender.value) * 60
+        seconds = currentValue
+        timerLabel.text = timeString(time: TimeInterval(seconds))
+        
     }
-    */
-
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        timerProgress.isHidden = false
+        slider.isHidden = true
+        startButton.isEnabled = false
+        startButton.isHidden = true
+        topTextLabel.text = "Take a nap"
+        
+        if isTimerRunning == false {
+            runTimer()
+            
+            //progress bar setting
+            self.progress = Progress(totalUnitCount: seconds)
+            timerProgress.progress = 0.0
+            self.progress.completedUnitCount = 0
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+                guard self.progress.isFinished == false else {
+                    timer.invalidate()
+                    return
+                }
+                self.progress.completedUnitCount += 1
+                self.timerProgress.setProgress(Float(self.progress.fractionCompleted), animated: true)
+            }
+        }
+        
+        slider.value = Float(seconds)/60
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(HomeVC.updateTimer)), userInfo: nil, repeats: true)
+        isTimerRunning = true
+        pauseButton.isEnabled = true
+        pauseButton.isHidden = false
+    }
+    
+    @IBAction func pauseButtonTapped(_ sender: UIButton) {
+        if self.resumeTapped == false {
+            timer.invalidate()
+            self.resumeTapped = true
+            resumeButton.isHidden = false
+            resetButton.isHidden = false
+            pauseButton.isHidden = true
+        }
+    }
+    @IBAction func resumeButtonTapped(_ sender: UIButton) {
+        if self.resumeTapped == true {
+            runTimer()
+            self.resumeTapped = false
+            resumeButton.isHidden = true
+            resetButton.isHidden = true
+            pauseButton.isHidden = false
+        }
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        seconds = Int64(slider.value) * 60
+        timerLabel.text = timeString(time: TimeInterval(slider.value * 60))
+        self.pauseButton.setTitle("Pause", for: .normal)
+        isTimerRunning = false
+        resumeTapped = false
+        topTextLabel.text = "Set the timer"
+        
+        //timer progress reset
+        timerProgress.progress = 0.0
+        self.progress.completedUnitCount = 0
+        
+        //button, slider, label setting
+        pauseButton.isEnabled = false
+        pauseButton.isHidden = true
+        startButton.isEnabled = true
+        startButton.isHidden = false
+        resetButton.isHidden = true
+        resumeButton.isHidden = true
+        slider.isHidden = false
+        timerProgress.isHidden = true
+    }
+    
+    @objc func updateTimer() {
+        if seconds < 1 {
+            timer.invalidate()
+            //Send alert to indicate "time's up!"
+        } else {
+            seconds -= 1
+            timerLabel.text = timeString(time: TimeInterval(seconds))
+        }
+    }
+    
+    func timeString(time:TimeInterval) -> String {
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format:"%02i:%02i", minutes, seconds)
+    }
+    
 }
---
+++ Kelompok 18 MC1/View Controller/HomeVCMusic.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVCMusic.swift
+//  Kelompok 18 MC1
+//
+//  Created by Yosua Marchel on 09/04/20.
+//  Copyright © 2020 Yosua Marchel. All rights reserved.
+//
+
+import UIKit
+
+class HomeVCMusic: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
